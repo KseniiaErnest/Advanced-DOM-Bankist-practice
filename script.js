35e5436ff7386202a61d463520de768a3998a68c
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-01-12 08:24:16 -0500

    Test commit

--- script.js
+++ script.js
@@ -30,3 +30,43 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////////
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+document.getElementsByClassName('btn');
+
+const message = document.createElement('div'); // return DOM element
+message.classList.add('cookie-message');
+message.textContent = 'blahblahblah';
+message.innerHTML =
+  'blahblahblah <button class="btn btn--close-cookie">Got it!</button>';
+header.prepend(message);
+header.append(message.cloneNode(true));
+
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+  message.remove();
+});
+
+message.style.backgroundColor = 'blue';
+message.style.width = '120%';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+const logo = document.querySelector('.nav_logo');
+console.log(logo.getAttribute('designer'));
+logo.setAttribute('comapny', 'Bankist');
+
+logo.classList.add('c');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+/////////// Test git
+console.log('Hello world!');
