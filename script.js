d3d49e15de2f7e89518da0098bcbaa0b08f74b1a
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-01-14 11:04:18 -0500

    Sticky Nav via scroll event

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ const section1 = document.querySelector('#section--1');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
+// Menu fade animation elements
+const nav = document.querySelector('.nav');
 
 const openModal = function (e) {
   e.preventDefault();
@@ -104,3 +106,35 @@ document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList
 
 })
 
+/////////////////////////////////////////////////////////////////////
+// Menu fade animation functionality
+const handleHover = function(e, opacity) {
+  if(e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelector('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+  
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+}
+
+// Passing 'argument' into handler
+nav.addEventListener('mouseover', handleHover.bind(0,5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+//////////////////////////////////////////////////////////////////////
+// Sticky navigation by adding 'sticky' class
+
+const initialCoord = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function() {
+
+  if(window.scrollY > initialCoord.top) {
+    nav.classList.add('sticky')
+  } else {
+    nav.classList.remove('sticky');
+  }
+})
